efe5669172ae686e0a0218952371bce4cd57aa2a
aria-afk <aria.lopez.github@gmail.com>
2024-12-27 14:38:47 -0800

    Fetch response from each task id

--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 // Struct representing relevant data from search_item_listing.json
@@ -14,12 +15,66 @@ type SearchItemIndexResponse struct {
 	TaskId string `json:"task_id"`
 }
 
-func main() {
+// Struct representing relevant data from slim.json
+// see: https://storage.googleapis.com/owlrepo/v1/uploads/97760776-0dfb-4f53-a110-7d1c40e35de0/slim.json
+type TaskIdReponse struct {
+	Payload []struct {
+		Screenshot struct {
+			Timestamp string `json:"timestamp"`
+			FileName  string `json:"name"`
+		} `json:"screenshot"`
+	} `json:"payload"`
+}
+
+func Scrape() {
 	// Retrieve the search_item_listing information
 	searchItemIndexUrl := "https://storage.googleapis.com/owlrepo/v1/queries/search_item_listing.json"
 	searchIndexResults := make([]SearchItemIndexResponse, 0)
 	err := getJsonAndDecode(searchItemIndexUrl, &searchIndexResults)
 	panicf(err, "Could not retrieve and decode search_item_listing.json")
+
+	// For each TaskId retrieve each screenshot's url and attached timestamp
+	var wg sync.WaitGroup
+	sem := make(chan int, 20)
+	taskIdErrors := make(chan error, len(searchIndexResults))
+	taskIdResponses := make(chan TaskIdReponse, len(searchIndexResults))
+
+	for i, sir := range searchIndexResults {
+		// TESTING: REMOVE ME
+		if i > 5 {
+			break
+		}
+		// -----------------
+		wg.Add(1)
+		sem <- 1
+		go func(taskId string) {
+			defer wg.Done()
+			defer func() { <-sem }()
+
+			taskIdUrl := "https://storage.googleapis.com/owlrepo/v1/uploads/" + taskId + "/slim.json"
+			tir := TaskIdReponse{}
+			err := getJsonAndDecode(taskIdUrl, &tir)
+			if err != nil {
+				taskIdErrors <- err
+				return
+			}
+
+			taskIdResponses <- tir
+		}(sir.TaskId)
+	}
+
+	wg.Wait()
+	close(taskIdErrors)
+	close(taskIdResponses)
+
+	if len(taskIdErrors) > (len(taskIdResponses) / 10) {
+		err := <-taskIdErrors
+		panicf(err, "fetching taskIdResponses had an error rate of > 10%")
+	}
+
+	for tir := range taskIdResponses {
+		fmt.Println(tir)
+	}
 }
 
 // Fetches expected json data from url and attempts to decode into target struct
@@ -37,4 +92,7 @@ func panicf(err error, message string) {
 	if err != nil {
 		panic(fmt.Sprintf("%s\n%e", message, err))
 	}
+}
+
+func main() {
 }
---
+++ scrapper/scrapper_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestScrapper(t *testing.T) {
+	Scrape()
+}
