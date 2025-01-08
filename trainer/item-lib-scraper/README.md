8166d1aaff4f51170fd43c8565b004ea3c2a4bbf
aria-afk <aria.lopez.github@gmail.com>
2025-01-08 14:59:45 -0800

    scrapper working

--- trainer/item-lib-scraper/README.md
+++ trainer/item-lib-scraper/README.md
@@ -4,3 +4,42 @@ There are so so many items in maple legends that can be searched via owl.
 
 This is a simple javascript script to navigate and generate a JSON list
 from the `maplelegends.com/lib` page.
+
+## To generate the items .json file
+
+`node scraper.js`
+
+## File structure
+
+```json
+{
+    "etc"": [
+        {
+            "libHref": "link to maplelegnds lib page for item",
+            "name": "name of item",
+            "type": "Etc"
+        }
+    ],
+    "equip": [
+        {
+            "libHref": "link to maplelegnds lib page for item",
+            "name": "name of item",
+            "type": "Equip"
+        }
+    ],
+    "use": [
+        {
+            "libHref": "link to maplelegnds lib page for item",
+            "name": "name of item",
+            "type": Use"
+        }
+    ],
+    "setup": [
+        {
+            "libHref": "link to maplelegnds lib page for item",
+            "name": "name of item",
+            "type": "Setup"
+        }
+    ]
+}
+```
--- trainer/item-lib-scraper/scraper.js
+++ trainer/item-lib-scraper/scraper.js
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
 
 const sleep = (ms) => new Promise(res => setTimeout(res, ms));
 
@@ -6,42 +7,62 @@ const sleep = (ms) => new Promise(res => setTimeout(res, ms));
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    // page=139 seems to be the first page with items and not just monsters.
-    await page.goto("https://maplelegends.com/lib/all?page=139");
-    await sleep(100);
+    async function parseByPage(pageNumber) {
+        await page.goto(`https://maplelegends.com/lib/all?page=${pageNumber}`);
+        await sleep(200);
+        return await page.$$eval(".table.text-center.table-bordered > tbody > tr", tr => {
+            return tr.map((trElem, i) => {
+                if (i == 0) return;
+                const tds = Array.from(trElem.querySelectorAll("td"));
 
-    const tableData = await page.$$eval(".table.text-center.table-bordered > tbody > tr", tr => {
-        return tr.map((trElem, i) => {
-            if (i == 0) return;
-            const tds = Array.from(trElem.querySelectorAll("td"));
-
-            return {
-                libHref: tds?.[1]?.querySelector("a").href,
-                name: tds?.[1]?.querySelector("a").innerText,
-                type: tds?.[2]?.innerText,
-            }
+                return {
+                    // NOTE: we can eventaully use the href to get description data 
+                    libHref: tds?.[1]?.querySelector("a").href,
+                    name: tds?.[1]?.querySelector("a").innerText,
+                    type: tds?.[2]?.innerText,
+                }
+            });
         });
-    });
-    console.log(tableData);
+    }
+    const allPageData = [];
+    // NOTE: this seems to be the range with all the non npc/cash/monster types 
+    // however may be better to just fetch everything and filter after.
+    let startingPage = 139;
+    const endPage = 885;
+    while (startingPage <= endPage) {
+        process.stdout.write(`scraping pages done [ ${startingPage} ] / [ ${endPage} ]`);
+        process.stdout.write(`\r`);
+        allPageData.push(...(await parseByPage(startingPage)));
+        startingPage++;
+    }
+
+    const resJson = {
+        "etc": [],
+        "equip": [],
+        "use": [],
+        "setup": [],
+    };
+
+    for (let i = 0; i < allPageData.length; i++) {
+        const pd = allPageData[i];
+        switch (pd?.type) {
+            case "Etc":
+                resJson.etc.push(pd);
+            case "Equip":
+                resJson.equip.push(pd);
+            case "Use":
+                resJson.use.push(pd);
+            case "Setup":
+                resJson.setup.push(pd);
+            default:
+                continue;
+        }
+    }
+
+    if (fs.existsSync("./mapleitems.json")) {
+        fs.unlinkSync("./mapleitems.json");
+    }
+    fs.writeFileSync("./mapleitems.json", JSON.stringify(resJson));
+
     await browser.close();
 })();
-// Headers
-/* TR structure:
- * #1 image container
- * <td>
-     <center>
-       <a href="linkToLibHref">
-         <object>-</object> info of image
-       </a>
-     </center>
- * </td>
- * #2 Name container
- * <td>
- *   <a href="linkToLibHref">
- *     {objectName}
- *   </a>
- * </td>
- * #3 Type container 
- * <td>{objectType}</td>
-*/
-
